6d9d5ae58d4ac7c34bb0073bb84168ca5948b7e6
“wnagguanshi” <wangguanshi@yoyodao.cc>
2022-09-24 18:54:29 +0800

    创建vue3项目学习websocket。

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+
+const routes = [
+        {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue'),
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Home
+        666666666666
+    </div>
+</template>
+
+<script>
+
+export default {
+   name: 'Home'
+}
+</script>
